9df04c042aa82df1763764c534d809b6c846a5ff
Andrew Nguyen <anguyen0615@gmail.com>
2022-04-22 23:26:41 -0500

    Update script.js
    Continued lessons till 50

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -227,23 +227,192 @@
 
  // 44. Object Methods
     
-    const andrew = {
-        // Property: value
-        firstName: 'Andrew',
-        lastName: 'Nguyen',
-        birthYear: 1997,
-        job: 'Software Engineer',
-        friends: ['Michael', 'Steven', 'Peter'],
-        hasDriversLicense: true,
+    // const andrew = {
+    //     // Property: value
+    //     firstName: 'Andrew',
+    //     lastName: 'Nguyen',
+    //     birthYear: 1997,
+    //     job: 'Software Engineer',
+    //     friends: ['Michael', 'Steven', 'Peter'],
+    //     hasDriversLicense: true,
 
-        // not DRY:
-        // calcAge: function(birthYear){
-        //     return 2037-birthYear;
-        // }
+    //     // not DRY:
+    //     // calcAge: function(birthYear){
+    //     //     return 2037-birthYear;
+    //     // }
 
-        calcAge: function(){
-            return 2037 - this.birthYear;
+    //     // calcAge: function(){
+    //     //     return 2037 - this.birthYear;
+    //     // }
+
+    //     calcAge: function(){
+    //         this.age = 2037 - this.birthYear;
+    //         return this.age;
+    //     },
+
+    //     getSummary: function(){
+    //         this.summary = `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
+    //         return this.summary
+    //     }
+    // };
+
+//    andrew.calcAge();
+
+    // If need to access age multiple times, shouldn't call this multiple time because
+    // of computation 
+    // console.log(andrew.calcAge());
+    // console.log(andrew.calcAge());
+    // console.log(andrew.calcAge());
+
+    // Challenge
+    // console.log(andrew.getSummary());
+
+
+// 45. Coding Challenge #3
+
+    // const mark = {
+    //     firstName: 'Mark',
+    //     lastName: 'Miller',
+    //     mass: 78,
+    //     heigh: 1.69,
+    //     calcBMI: function(){
+    //         this.BMI = this.mass / this.heigh **2;
+    //         return this.BMI;
+    //     }
+    // };
+
+    // const john = {
+    //     firstName: 'John',
+    //     lastName: 'Smith',
+    //     mass: 92,
+    //     heigh: 1.95,
+
+    //     calcBMI: function(){
+    //         this.BMI = this.mass / this.heigh **2;
+    //         return this.BMI;
+    //     }
+    // };
+
+    // mark.calcBMI();
+    // john.calcBMI();
+
+    // console.log(`${john.firstName} ${john.lastName}'s BMI (${john.BMI}) is ${john.BMI > mark.BMI?'higher':'lower'} than ${mark.firstName} ${mark.lastName}'s (${mark.BMI})!`)
+
+// 46. Iteration: The For Loop
+
+    // For loops keep running while condition is TRUE;
+    // It checks before running it
+    // for(let rep = 1; rep <= 10; rep++ ){
+    //     console.log(`Lifting weights repetition ${rep}`)
+    // };
+
+
+// 47. Loop arrays, breaking and continuing
+    // const andrewArray = [
+    //     'Andrew',
+    //     'Nguyen',
+    //     2037 - 1991,
+    //     'Software Engineer',
+    //     ['Brady', 'Travis', 'Bob']
+    // ];
+
+    // const types = [];
+
+    // for(let i = 0; i < andrewArray.length; i++){
+    //     console.log(andrewArray[i], typeof andrewArray[i]);
+
+    //     // Filling types array
+    //     // types[i] = typeof andrewArray[i];
+
+    //     types.push(typeof andrewArray[i]);
+    // }
+
+    // console.log(types)
+
+    // const years = [1991, 2007, 1969, 2020];
+    // const ages = [];
+    // for(let i = 0; i < years.length; i++){
+    //     ages.push( 2037 - years[i] );
+    // }
+
+    // console.log(ages)
+
+    // Continue and Break statement
+    // console.log("Continue")
+    // for(let i = 0; i < andrewArray.length; i++){
+        
+    //     if(typeof andrewArray[i] !== 'string') continue;
+    //     console.log(andrewArray[i], typeof andrewArray[i]);
+    // }
+    // console.log("Break")
+    // for(let i = 0; i < andrewArray.length; i++){
+        
+    //     if(typeof andrewArray[i] === 'number') break;
+    //     console.log(andrewArray[i], typeof andrewArray[i]);
+    // }
+
+// 48. Looping backwards and Loops in Loops
+    // const andrewArray = [
+    //     'Andrew',
+    //     'Nguyen',
+    //     2037 - 1991,
+    //     'Software Engineer',
+    //     ['Brady', 'Travis', 'Bob'],
+    //     true
+    // ];
+
+    // for(let i = andrewArray.length - 1; i >= 0; i--){
+    //     console.log(i, andrewArray[i]);
+    // }
+
+    // for(let exercise = 1; exercise < 4; exercise++){
+    //     console.log(`--Starting exercise ${exercise} --`)
+    //     for(let rep = 1; rep < 6; rep++){
+    //         console.log(`Lifting weights repetition ${rep}`)
+    //     }
+    // }
+
+// 49. While Loop
+    // console.log("--For Loop--")
+    // for(let rep = 1; rep <= 10; rep++ ){
+    //     console.log(`Lifting weights repetition ${rep}`)
+    // };
+
+    // let rep = 1;
+    // console.log("--While Loop--")
+    // while(rep <= 10){
+    //     console.log(`Lifting weights repetition ${rep}`)
+    //     rep++;
+    // };
+
+    // let dice = Math.trunc(Math.random() * 6) + 1;
+
+    // Down side is if it is a 6 right at the beginning then the loop wouldn't be touched
+    // while(dice !== 6){
+    //     console.log(`You rolled a ${dice}.`);
+    //     dice = Math.trunc(Math.random() * 6) + 1;
+    //     if(dice === 6){
+    //         console.log(`You finally rolled a ${dice}!!`);
+    //     }
+    // }
+
+// 50. Coding challenge #4
+    const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+    const tips = [];
+    const total = [];
+
+    const calcTip = (bill) => {
+        if(bill >= 50 && bill <= 300){
+            return bill * 0.15;
+        }
+        else{
+            return bill * 0.20;
         }
     };
 
-    console.log(andrew.calcAge());
+    for(let i = 0; i < bills.length; i++){
+        tips.push[calcTip(bills[i])];
+        console.log(tips)
+    }
+
+    
