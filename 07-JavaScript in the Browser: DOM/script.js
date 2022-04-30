cf91350dc9a6ae3d9f92525d815426faa17bcba8
Andrew Nguyen <anguyen0615@gmail.com>
2022-04-29 23:35:46 -0500

    Update script.js
    Continue number guessing game

--- 07-JavaScript in the Browser: DOM/script.js
+++ 07-JavaScript in the Browser: DOM/script.js
@@ -4,3 +4,16 @@
 const message = document.querySelector(".message").textContent;
 
 console.log(message);
+
+// 72. Selecting and Manipulating elements
+
+document.querySelector(".message").textContent = "👌 Correct Number";
+
+const message2 = document.querySelector(".message").textContent;
+console.log(message2);
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value;
+document.querySelector(".guess").value = 23;
