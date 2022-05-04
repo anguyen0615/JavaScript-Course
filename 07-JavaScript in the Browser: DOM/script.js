e8f0a91c5401f7306888d1b5b9fe4c8bfeb118c3
Andrew Nguyen <anguyen0615@gmail.com>
2022-05-03 23:18:09 -0500

    Course 73
    Learning about handling click events. So far it's just selecting the element and assigning event listener to the click with a function.

--- 07-JavaScript in the Browser: DOM/script.js
+++ 07-JavaScript in the Browser: DOM/script.js
@@ -1,6 +1,6 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
-
+/*
 const message = document.querySelector(".message").textContent;
 
 console.log(message);
@@ -17,3 +17,12 @@ document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value;
 document.querySelector(".guess").value = 23;
+*/
+
+// 73. Handling Click Event
+const checkBtn = document.querySelector(".check");
+const guess = document.querySelector(".guess");
+
+checkBtn.addEventListener("click", function () {
+  console.log(guess.value);
+});
